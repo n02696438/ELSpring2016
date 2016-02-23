40b67e6502d0d5f4c8acef99ecfa78a364ff0488
n02696438 <n02696438@hawkmail.newpaltz.edu>
2016-02-22 21:51:03 -0500

    added assignment3.pdf to misc and added comments to assignment 3 code

--- code/createDB.py
+++ code/createDB.py
@@ -2,9 +2,10 @@ import sqlite3 as mydb
 import sys
 
 
-
+#Connects to database testTime.db, creates it if not existing
 con = mydb.connect('testTime.db')
 c = con.cursor()
 
+#creates table inside database for use
 c.execute('''CREATE TABLE dateTime (xdate TEXT, xtime TEXT);''')
 con.commit()
